a8232240346dc1a3603555028e34c4dd7c7bb493
Renan Carneiro <carneiro.renan5@gmail.com>
2022-08-28 00:57:21 -0300

    feat: interfaces

---
+++ src/app/interfaces/IProduct.ts
@@ -0,0 +1,33 @@
+import { Types } from 'mongoose'
+
+export interface IProduct {
+  title: string
+  description: string
+  department: string
+  bar_codes: string
+  brand: string
+  price: number
+  qtd_stock: number
+  stock_control_enabled?: boolean
+}
+
+export interface IProductResponse {
+  title: string
+  description: string
+  department: string
+  bar_codes: string
+  brand: string
+  price: number
+  qtd_stock: number
+  _id: Types.ObjectId
+  stock_control_enabled?: boolean
+  __v?: number
+}
+
+export interface IQueryGet {
+  qtd_stock?: object
+  department?: string
+  brand?: string
+  limit?: number
+  page?: number
+}
